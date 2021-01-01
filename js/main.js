6d31fecdae6c533435da293d82e3f8a2aacdd1ca
Franciom Veloso <franciomveloso@gmail.com>
2021-01-01 20:50:50 -0300

    criado functions no arquivo main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimennto"));
+    //alert("Obrigado por Clicar");
+}
+
+function redirecionar(){
+    //window.open abre em outra guia.
+    window.open("https://www.google.com/");
+    
+    //window.location.href abre na guia.
+    //window.location.href = "https://www.google.com/"
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML = "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+   elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+/*function soma(n1, n2){
+    return n1, n2;
+}
+
+console.log(soma);
+*/
 /*var count;
 for (count = 0; count <=5; count++){
     alert(count);
