b31cccf37537a91ce1b333f178daad4363f24a49
Franciom Veloso <franciomveloso@gmail.com>
2021-01-01 18:23:38 -0300

    criado arquivos index.html e pasta js/main.js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*var count;
+for (count = 0; count <=5; count++){
+    alert(count);
+};*/
+
+/*var count = 1;
+while (count <= 5){
+    console.log(count);
+    count++
+}*/
+
+
+/*var idade = prompt("Qual a sua Idade");
+if (idade >= 18 ){
+    alert("Maior de Idade");
+}else {
+    alert("Menor de Idade");
+};
+*/
+
+// Para comentar em Javascript utiliza-se o // ou */
+//var fruta = {nome:"Maçã", cor:"v ermelha"};
+//console.log(fruta);
+
+//var lista = ["Maçã", "Melão", "Melancia", "Pera", "Uva"];
+//console.log(lista.length);
+
+/* var nome = "Franciom Veloso";
+var idade = 24;
+var idade2 = 10;
+var frase = "Japão é a melhor seleção do mundo";
+//alert (nome + "tem" + idade + "anos");
+//alert (idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão" , "Brasil"));
+*/
